f1659755d8b6d31bd38cf5a9aa036ba23850a03a
Ahmed-pc <perfect_ahmed@yahoo.com>
2020-06-18 23:56:50 +0300

    features: handle offline requests

--- src/client/js/handleFormSubmit.js
+++ src/client/js/handleFormSubmit.js
@@ -1,18 +1,18 @@
 // make sure to export the function you would like to import
 
-const errorElement =   document.getElementById('errorFeedback');
-const evaluationElement =   document.getElementById('evaluation');
-const evaluationElementWrapper =   document.getElementById('evaluationWrapper');
+const errorElement = document.getElementById('errorFeedback');
+const evaluationElement = document.getElementById('evaluation');
+const evaluationElementWrapper = document.getElementById('evaluationWrapper');
 
 function handleFormSubmit(event) {
     errorElement.innerText = '';
     event.preventDefault();
 
     // take the value entered in the input
-    let textInput = document.getElementById('textInput').value ;
+    let textInput = document.getElementById('textInput').value;
 
     // validate the form input | it should not be empty and maximum of 3000 chars.
-    if(textInput.length > 3000 || textInput.length < 4 ){
+    if (textInput.length > 3000 || textInput.length < 4) {
         // update error layout
         errorElement.innerText = 'Text can not be less than 7 chars or more than 3000 chars.';
         return;
@@ -20,13 +20,13 @@ function handleFormSubmit(event) {
 
     // send it to the server ( to evaluate through the API )
 
-    postText('/evaluate',{textInput: textInput})
-        .then( (response) => {
+    postText('/evaluate', {textInput: textInput})
+        .then((response) => {
             // update layout with result from server:
-            let HTML = "" ;
+            let HTML = "";
 
-            Object.keys(response.evaluationData).forEach(function(key) {
-                HTML += `<b>${key}</b>: ${response.evaluationData[key]} <br/>` ;
+            Object.keys(response.evaluationData).forEach(function (key) {
+                HTML += `<b>${key}</b>: ${response.evaluationData[key]} <br/>`;
             });
 
             evaluationElement.innerHTML = HTML;
@@ -37,22 +37,26 @@ function handleFormSubmit(event) {
 }
 
 const postText = async (url = '', data = {}) => {
-    // use Fetch API to Post data:
-    const request = await fetch(url, {
-        method: 'POST',
-        credentials: 'same-origin',
-        headers: {
-            'content-type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    });
-
-    // read the response of this request:
-    try {
-        return await request.json();
-    } catch (e) {
-        errorElement.innerHTML = 'Error: ' + e.message;
+    // use Fetch API to Post data: // use try here for internet failing error.
+    try{
+        const request = await fetch(url, {
+            method: 'POST',
+            credentials: 'same-origin',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        });
+        // read the response of this request:
+        try {
+            return await request.json();
+        } catch (e) {
+            errorElement.innerHTML = 'Error: ' + e.message;
+        }
+    }catch (e) {
+        errorElement.innerHTML = 'Error: ' + e.message + ' | Please check your internet connection.';
     }
+
 };
 
-export { handleFormSubmit };
+export {handleFormSubmit};
