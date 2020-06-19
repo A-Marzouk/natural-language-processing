be2e795452a883b3b97d01558866cd4d40d6d03f
Ahmed-pc <perfect_ahmed@yahoo.com>
2020-06-19 23:02:55 +0300

    Feat: use different API endpoints

--- src/client/js/handleFormSubmit.js
+++ src/client/js/handleFormSubmit.js
@@ -11,10 +11,10 @@ function handleFormSubmit(event) {
     // take the value entered in the input
     let textInput = document.getElementById('textInput').value;
 
-    // validate the form input | it should not be empty and maximum of 3000 chars.
-    if (textInput.length > 3000 || textInput.length < 4) {
+    // validate the form input | should be valid url.
+    if (!validateUrl(textInput)) {
         // update error layout
-        errorElement.innerText = 'Text can not be less than 7 chars or more than 3000 chars.';
+        errorElement.innerText = 'Not a valid URL.';
         return;
     }
 
@@ -23,17 +23,47 @@ function handleFormSubmit(event) {
     Client.postData('/evaluate', {textInput: textInput})
         .then((response) => {
             // update layout with result from server:
-            let HTML = "";
-
-            Object.keys(response.evaluationData).forEach(function (key) {
-                HTML += `<b>${key}</b>: ${response.evaluationData[key]} <br/>`;
-            });
-
-            evaluationElement.innerHTML = HTML;
+            evaluationElement.innerHTML = formatHTML(response.evaluationData);
             evaluationElementWrapper.style.display = 'block';
         });
 
 
+}
+
+function formatHTML(data){
+    let HTML = '' ;
+
+    let text = data.text;
+    let results = data.results;
+
+    HTML += `<b>Text: </b> ${text} <br/>`;
+    HTML += `<hr/>`;
+    HTML += `<b>Results: </b><br/>`;
+
+    // the results are array of objects: [endpoint and result]
+
+    for (let i = 0; i < results.length ; i++){
+        HTML += `<b>Endpoint: </b> ${results[i].endpoint} <br/>`;
+
+        // result is an object of key value pairs
+        HTML += '<div class="p-2">';
+        HTML += `<b>Results: </b><br/>`;
+        let resultsObject = results[i].result;
+            for(let key in resultsObject){
+                if(resultsObject[key].length !== 0){
+                    HTML += `<b>${key}: </b> ${resultsObject[key]} <br/>`;
+                }
+            }
+        HTML += '</div>';
+
+    }
+
+
+    return HTML ;
+}
+
+function validateUrl(value) {
+    return /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/i.test(value);
 }
 
 module.exports = {
